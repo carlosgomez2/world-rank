6ede4d4638365ca10df42aeefd2754b7c0ef48c8
Carlos Gómez <carlosgomez.deb@gmail.com>
2021-02-12 20:55:14 -0600

    :sparkles: - now countries can be sorted by name or population

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,27 @@
-import Layout from '../components/Layout/Layout'
+import CountriesTable from '../components/CountriesTable/CountriesTable';
+import Layout from '../components/Layout/Layout';
+import SearchInput from '../components/SearchInput/SearchInput';
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
-  return <Layout>main</Layout>;
-}
+export default function Home({countries}) {
+  return (
+    <Layout>
+      <div className={styles.count}>Found {countries.length} countries.</div>
+
+      <SearchInput placeholder="Filter by Name, Region or Subregion" />
+
+      <CountriesTable countries={countries} />
+    </Layout>
+  );
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://restcountries.eu/rest/v2/all');
+  const countries = await res.json();
+
+  return {
+    props: {
+      countries,
+    },
+  };
+};
